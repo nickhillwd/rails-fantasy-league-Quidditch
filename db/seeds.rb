9a7acb2a53818c59967a5c224895901dda3d04eb
nickhillwd <nickhillwd@gmail.com>
2015-12-09 14:42:07 +0000

    ready to go

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
-# Team.delete_all
+Team.delete_all
 
-#  Team.create!(name: 'Moutohora Macaws', founded: '1 March 1872', mascot: 'Sparky')
-#  Team.create!(name: 'Ballycastle Bats', founded: '6 November 1887', mascot: 'Barny the Fruitbat')
-#  Team.create!(name: 'Kenmare Kestrels', founded: '24 May 1909', mascot: 'Kez')
+ Team.create!(name: 'Moutohora Macaws', founded: '1 March 1872', mascot: 'Sparky')
+ Team.create!(name: 'Ballycastle Bats', founded: '6 November 1887', mascot: 'Barny the Fruitbat')
+ Team.create!(name: 'Kenmare Kestrels', founded: '24 May 1909', mascot: 'Kez')
+ Team.create!(name: 'Las Cucarachas', founded: '24 May 1356', mascot: 'Colin the Cockroach')
 
 Player.delete_all
 
