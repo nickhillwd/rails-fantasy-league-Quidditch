a357a78617bb14e0b382344dee14031ba3dc2b98
nickhillwd <nickhillwd@gmail.com>
2015-12-09 15:37:22 +0000

    added team column to players table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209135536) do
+ActiveRecord::Schema.define(version: 20151209153514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151209135536) do
     t.integer  "snitch_catch_count"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.integer  "team_id"
   end
 
   create_table "teams", force: :cascade do |t|
