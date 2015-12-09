8d40b784275f74c85e306645432bfa3904288dc8
nickhillwd <nickhillwd@gmail.com>
2015-12-09 15:18:05 +0000

    added views restful routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,4 +4,39 @@ class PlayersController < ApplicationController
     @players = Player.all
   end
 
+  def new
+    @player = Player.new
+  end
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+  def destroy
+    player = Player.find(params[:id])
+    player.destroy
+    redirect_to(players_path)
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    player = Player.find(params[:id])
+    player.update(player_params)
+    redirect_to(players_path)
+  end
+
+  def create
+    Player.create(player_params)
+    redirect_to(players_path)
+  end
+
+  private
+
+    def player_params
+      params.require(:player).permit(:player_name, :position, :player_number, :snitch_catch_count)
+    end
+
 end
